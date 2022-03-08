6607a7834df282e41b7f973ce73ada2091d4e72b
aviokristof <91827782+aviokristof@users.noreply.github.com>
2022-03-08 23:15:26 +0100

    Add files via upload

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.Start = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.Phi = new System.Windows.Forms.RadioButton();
+            this.Theta = new System.Windows.Forms.RadioButton();
+            this.Psi = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.BackColor = System.Drawing.Color.Lime;
+            this.Start.Location = new System.Drawing.Point(362, 610);
+            this.Start.Margin = new System.Windows.Forms.Padding(2);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(100, 41);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = false;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.BackColor = System.Drawing.Color.Red;
+            this.Stop.Location = new System.Drawing.Point(853, 610);
+            this.Stop.Margin = new System.Windows.Forms.Padding(2);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(100, 41);
+            this.Stop.TabIndex = 1;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = false;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(124, 235);
+            this.chart1.Margin = new System.Windows.Forms.Padding(2);
+            this.chart1.Name = "chart1";
+            this.chart1.Padding = new System.Windows.Forms.Padding(82, 89, 0, 0);
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Phi";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "Theta";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "Psi";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(531, 322);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = " ";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(680, 235);
+            this.chart2.Margin = new System.Windows.Forms.Padding(2);
+            this.chart2.Name = "chart2";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Legend = "Legend1";
+            series4.Name = "Phi";
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series5.Legend = "Legend1";
+            series5.Name = "Theta";
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series6.Legend = "Legend1";
+            series6.Name = "Psi";
+            this.chart2.Series.Add(series4);
+            this.chart2.Series.Add(series5);
+            this.chart2.Series.Add(series6);
+            this.chart2.Size = new System.Drawing.Size(531, 322);
+            this.chart2.TabIndex = 3;
+            this.chart2.Text = "chart2";
+            // 
+            // Phi
+            // 
+            this.Phi.AutoSize = true;
+            this.Phi.BackColor = System.Drawing.Color.Transparent;
+            this.Phi.Location = new System.Drawing.Point(104, 94);
+            this.Phi.Margin = new System.Windows.Forms.Padding(2);
+            this.Phi.Name = "Phi";
+            this.Phi.Size = new System.Drawing.Size(49, 21);
+            this.Phi.TabIndex = 4;
+            this.Phi.TabStop = true;
+            this.Phi.Text = "Phi";
+            this.Phi.UseVisualStyleBackColor = false;
+            this.Phi.CheckedChanged += new System.EventHandler(this.Phi_CheckedChanged);
+            // 
+            // Theta
+            // 
+            this.Theta.AutoSize = true;
+            this.Theta.BackColor = System.Drawing.Color.Transparent;
+            this.Theta.Location = new System.Drawing.Point(103, 133);
+            this.Theta.Margin = new System.Windows.Forms.Padding(2);
+            this.Theta.Name = "Theta";
+            this.Theta.Size = new System.Drawing.Size(66, 21);
+            this.Theta.TabIndex = 5;
+            this.Theta.TabStop = true;
+            this.Theta.Text = "Theta";
+            this.Theta.UseVisualStyleBackColor = false;
+            this.Theta.CheckedChanged += new System.EventHandler(this.Theta_CheckedChanged);
+            // 
+            // Psi
+            // 
+            this.Psi.AutoSize = true;
+            this.Psi.BackColor = System.Drawing.Color.Transparent;
+            this.Psi.Location = new System.Drawing.Point(104, 172);
+            this.Psi.Margin = new System.Windows.Forms.Padding(2);
+            this.Psi.Name = "Psi";
+            this.Psi.Size = new System.Drawing.Size(48, 21);
+            this.Psi.TabIndex = 6;
+            this.Psi.TabStop = true;
+            this.Psi.Text = "Psi";
+            this.Psi.UseVisualStyleBackColor = false;
+            this.Psi.CheckedChanged += new System.EventHandler(this.Psi_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.Navy;
+            this.label1.Location = new System.Drawing.Point(525, 33);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(330, 113);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "AHRS";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSize = true;
+            this.BackgroundImage = global::WindowsFormsApp3.Properties.Resources._1280x720;
+            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Psi);
+            this.Controls.Add(this.Theta);
+            this.Controls.Add(this.Phi);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Start);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.RadioButton Phi;
+        private System.Windows.Forms.RadioButton Theta;
+        private System.Windows.Forms.RadioButton Psi;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,352 @@
+﻿/*
+ * Attitude and heading reference system using complementary filter
+ * by Krzysztof Ragan
+ * 
+ * 
+ */
+
+using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO;
+using System.IO.Ports;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        /*
+         * Global variables
+        */
+        double Ax, Ay, Az, Gx, Gy, Gz, Mx, My, Mz, Gx_radians, Gy_radians, Gz_radians;
+        double phi_acc, theta_acc, phi_gyro, theta_gyro, psi_gyro;
+        double sum_phi, sum_theta, phi_kom, theta_kom, sum_psi, psi_kom, psi_kom_deg;
+        double phi_acc_deg, theta_acc_deg, phi_kom_deg, theta_kom_deg, sum_phi_deg, sum_theta_deg,psi_mag,psi_mag_deg, sum_psi_deg;
+        double phi_gyro_deg, theta_gyro_deg, psi_gyro_deg;
+        double chart1_data, chart2_data;
+        double time = 0;
+        double result;
+        string data=null;
+        double chart_phi_flag = 0, chart_theta_flag = 0, chart_psi_flag = 0;
+        string[]buffor=new string [6];
+        string[] frame = new string[8];
+        int test = 0;
+        //complementary filter coefficients
+        static double t = 0.1;
+        static double T_phi = 0.3;
+        static double T_theta = 0.3;
+        static double T_psi = 0.3;
+        double alpha_phi = Math.Exp(-t / T_phi);
+        double alpha_theta = Math.Exp(-t / T_theta);
+        double alpha_psi = Math.Exp(-t / T_psi);
+        SerialPort myserialPort = new SerialPort("COM4",38400,Parity.None,8,StopBits.One);
+        private Thread thread;
+        /*
+         * End of global variables         
+        */
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Start_Click(object sender, EventArgs e)
+        {
+            /*
+             * Starting program via button "Start"
+             */
+            chart_phi_flag = 1;
+            myserialPort.Open();
+            StartThread();
+            timer1.Start();
+        }
+        private void Stop_Click(object sender, EventArgs e)
+        {      
+            /*
+             * Stops program via button "Stop"
+             */
+            myserialPort.Close();
+            thread.Abort();
+            timer1.Stop();
+            test = 0;
+        }
+        
+
+
+
+
+
+        public void StartThread ()
+        {
+            /*
+             * Starts function "ThreadLoop" and sets labels parameters
+             */
+
+            label1.Font = new Font("Trebuchet MS", 60, FontStyle.Bold);
+            this.chart1.Titles.Add("Filtered");
+            chart1.Titles[0].Font = new Font("Trebuchet MS", 16, FontStyle.Bold);
+            this.chart2.Titles.Add("Not filtered");
+            chart2.Titles[0].Font = new Font("Trebuchet MS", 16, FontStyle.Bold);
+            //creates and starts the thread
+            thread = new Thread(ThreadLoop);
+            thread.Start();
+        }
+        public void ThreadLoop()
+        {
+            /*
+             * Main thread
+             */
+            while (myserialPort.IsOpen)
+            {
+                /*
+                 * Loop reads data via UART from STM32 microcontroller
+                 * Calculate Euler Angles using complementary filter
+                 */
+                
+                while (test<9)
+                {
+                    /*
+                     * Delay loop
+                     */
+                    data = myserialPort.ReadTo("\n");
+                    test++;
+                }
+
+                time = time + 0.1;
+                time = Math.Round(time,1);
+                
+                /*
+                 * Reading frame from UART and changing it to double variables
+                 */
+
+                data = myserialPort.ReadTo("\n");
+                frame = data.Split(';');
+                Ax = Convert.ToDouble(frame[0], new CultureInfo("en-US"));// raw value of acceleration in axis X
+                Ay = Convert.ToDouble(frame[1], new CultureInfo("en-US"));// raw value of acceleration in axis Y
+                Az = Convert.ToDouble(frame[2], new CultureInfo("en-US"));// raw value of acceleration in axis Z
+                Gx = Convert.ToDouble(frame[3], new CultureInfo("en-US"));// raw value of angular velocity in axis X
+                Gy = Convert.ToDouble(frame[4], new CultureInfo("en-US"));// raw value of angular velocity in axis Y
+                Gz = Convert.ToDouble(frame[5], new CultureInfo("en-US"));// raw value of angular velocity in axis Z
+                Mx = Convert.ToDouble(frame[6], new CultureInfo("en-US"));// raw value of magnetometer heading in axis X
+                My = Convert.ToDouble(frame[7], new CultureInfo("en-US"));// raw value of magnetometer heading in axis Y
+                Mz = Convert.ToDouble(frame[8], new CultureInfo("en-US"));// raw value of magnetometer heading in axis Z
+
+                /*
+                 * End of reading from UART
+                 */
+                
+                /*
+                 * Recalculating angular velocity values from degrees to radians
+                 */
+                Gx_radians = Gx * (Math.PI/180);//recalculated angular velocity in axis X
+                Gy_radians = Gy * (Math.PI / 180);//recalculated angular velocity in axis Y
+                Gz_radians = Gz * (Math.PI / 180);//recalculated angular velocity in axis Z
+
+                /*
+                 * End of recalculating angular velocity values from degrees to radians
+                 */
+
+
+                /*
+                 * Complementary filter 
+                 */
+                phi_acc = Math.Atan2(Ay, Az);//atan2(ay/az)
+                theta_acc = Math.Atan2(-(Ax), ((Ay * Math.Sin(phi_kom)) + (Az * Math.Cos(phi_kom))));
+
+                phi_gyro = Gx_radians + (Gy_radians * Math.Sin(phi_kom) * Math.Tan(theta_kom)) + (Gz_radians * Math.Cos(phi_kom) * Math.Tan(theta_kom));
+                theta_gyro = (Gy_radians * Math.Cos(phi_kom))- (Gz_radians * Math.Sin(theta_kom));
+                psi_gyro = Gx_radians + (Gy_radians * Math.Sin(phi_kom) * (1 / Math.Cos(theta_kom))) + (Gz_radians * Math.Cos(phi_kom) * (1 / Math.Cos(theta_kom)));
+
+                psi_mag = Math.Atan2((Mz*Math.Sin(phi_kom))-(My*Math.Cos(phi_kom)),(Mx*Math.Cos(theta_kom))+(My*Math.Sin(theta_kom) *Math.Sin(phi_kom))+(Mz*Math.Sin(theta_kom) *Math.Cos(phi_kom)));
+
+
+                sum_phi = phi_acc + (phi_gyro * T_phi);
+                sum_theta = theta_acc + (theta_gyro * T_theta);
+                sum_psi = psi_mag + (psi_gyro * T_psi);
+
+                phi_kom = (phi_kom * alpha_phi) + ((1 - alpha_phi) * sum_phi);
+                theta_kom = (theta_kom * alpha_theta) + ((1 - alpha_theta) * sum_theta);
+                psi_kom = (psi_kom * alpha_psi) + ((1 - alpha_psi) * sum_psi);
+
+                /*
+                 * End of complementary filter calculations
+                 */
+
+                /*
+                 * Recalculating values from radians to degrees to plot in charts
+                 */
+
+                phi_acc_deg = phi_acc * (180 / Math.PI);//radians to degrees
+                theta_acc_deg = theta_acc * (180 / Math.PI);//radians to degrees
+                psi_mag_deg = psi_mag * (180 / Math.PI);//radians to degrees
+
+                phi_gyro_deg = phi_gyro * (180 / Math.PI);//radians to degrees
+                theta_gyro_deg = theta_gyro * (180 / Math.PI);//radians to degrees
+                psi_gyro_deg = psi_gyro * (180 / Math.PI);//radians to degrees
+
+                sum_phi_deg = sum_phi * (180 / Math.PI);//radians to degrees
+                sum_theta_deg = sum_theta * (180 / Math.PI);//radians to degrees
+                sum_psi_deg= sum_psi* (180 / Math.PI);//radians to degrees
+
+                //final values from complementary filter
+                phi_kom_deg = phi_acc * (180 / Math.PI);//radians to degrees
+                theta_kom_deg = theta_acc * (180 / Math.PI);//radians to degrees
+                psi_kom_deg = psi_kom * (180 / Math.PI);//radians to degrees
+
+                /*
+                 * End of recalculating values from radians to degrees to plot in charts
+                 */
+
+                Thread.Sleep(100);
+               
+            }
+            
+         
+        }
+
+
+        private void Phi_CheckedChanged(object sender, EventArgs e)
+        {
+            /*
+             * Radio button "Phi" changing ploting series on charts to "Phi" series. Prints phi Euler angle values in degrees.
+             */
+
+            chart_phi_flag = 1;
+
+        }
+
+        private void Theta_CheckedChanged(object sender, EventArgs e)
+        {
+            /*
+             * Radio button "Theta" changing ploting series on charts to "Theta" series. Prints theta Euler angle values in degrees.
+             */
+            chart_theta_flag = 1;
+
+        }
+
+        private void Psi_CheckedChanged(object sender, EventArgs e)
+        {
+            /*
+             * Radio button "Psi" changing ploting series on charts to "Psi" series. Prints psi Euler angle values in degrees.
+             */
+            chart_psi_flag = 1;
+
+        }
+                                    
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            /*
+             * Timer used to plot variables in charts
+             */
+
+            // Chart 1
+            chart1.ChartAreas[0].AxisX.Title = "Time [ s ]"; // Chart1 X axis Title name
+            chart1.ChartAreas[0].AxisY.Title = "Degrees [ ° ]"; // Chart1 Y axis Title name
+            chart1.ChartAreas[0].AxisX.TitleFont = new Font("Trebuchet MS", 10, FontStyle.Bold); // Chart1 X axis Title Font
+            chart1.ChartAreas[0].AxisY.TitleFont = new Font("Trebuchet MS", 10, FontStyle.Bold); // Chart1 Y axis Title Font
+            chart1.ChartAreas[0].RecalculateAxesScale();
+
+            if (chart_phi_flag == 1)
+            {
+                chart1.Series["Phi"].Points.AddXY(time,phi_kom_deg);
+                chart1.Series["Phi"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            }
+            if (chart_theta_flag == 1)
+            {
+                chart1.Series["Theta"].Points.AddXY(time, theta_kom_deg);
+                chart1.Series["Theta"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            }
+            if (chart_psi_flag == 1)
+            {
+                chart1.Series["Psi"].Points.AddXY(time, psi_kom_deg);
+                chart1.Series["Psi"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            }
+
+            if (chart_phi_flag == 0 && chart_theta_flag == 0 && chart_psi_flag == 0)
+            {
+                chart1.Series["Phi"].Points.RemoveAt(0);
+            }
+            if (chart_phi_flag == 1 && (chart1.Series["Phi"].Points.Count > 100 || chart1.Series["Theta"].Points.Count > 100 || chart1.Series["Psi"].Points.Count > 100))
+            {
+                chart1.Series["Phi"].Points.RemoveAt(0);
+            }
+            if (chart_theta_flag == 1 && (chart1.Series["Theta"].Points.Count > 100 || chart1.Series["Theta"].Points.Count > 100 || chart1.Series["Psi"].Points.Count > 100))
+            {
+                chart1.Series["Theta"].Points.RemoveAt(0);
+            }
+            if ( chart_psi_flag == 1 && (chart1.Series["Psi"].Points.Count > 100 || chart1.Series["Theta"].Points.Count > 100 || chart1.Series["Psi"].Points.Count > 100))
+            {
+                chart1.Series["Psi"].Points.RemoveAt(0);
+            }
+
+            // Chart 2
+
+            chart2.ChartAreas[0].AxisX.Title = "Time [ s ]";// Chart1 X axis Title name
+            chart2.ChartAreas[0].AxisY.Title = "Degrees [ ° ]";// Chart1 Y axis Title name
+            chart2.ChartAreas[0].AxisX.TitleFont = new Font("Trebuchet MS", 10, FontStyle.Bold); // Chart1 X axis Title font
+            chart2.ChartAreas[0].AxisY.TitleFont = new Font("Trebuchet MS", 10, FontStyle.Bold); // Chart1 Y axis Title font
+            chart2.ChartAreas[0].RecalculateAxesScale();
+
+            if (chart_phi_flag == 1)
+            {
+                chart2.Series["Phi"].Points.AddXY(time, phi_gyro_deg);
+                chart2.Series["Phi"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            }
+            if (chart_theta_flag == 1)
+            {
+                chart2.Series["Theta"].Points.AddXY(time, theta_gyro_deg);
+                chart2.Series["Theta"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            }
+            if (chart_psi_flag == 1)
+            {
+                chart2.Series["Psi"].Points.AddXY(time, psi_gyro_deg);
+                chart2.Series["Psi"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            }
+
+            if (chart_phi_flag == 0 && chart_theta_flag == 0 && chart_psi_flag == 0)
+            {
+                chart2.Series["Phi"].Points.RemoveAt(0);
+            }
+            if (chart_phi_flag == 1 && (chart2.Series["Phi"].Points.Count > 100 || chart2.Series["Theta"].Points.Count > 100 || chart2.Series["Psi"].Points.Count > 100))
+            {
+                chart2.Series["Phi"].Points.RemoveAt(0);
+            }
+            if (chart_theta_flag == 1 && (chart2.Series["Theta"].Points.Count > 100 || chart2.Series["Theta"].Points.Count > 100 || chart2.Series["Psi"].Points.Count > 100))
+            {
+                chart2.Series["Theta"].Points.RemoveAt(0);
+            }
+            if (chart_psi_flag == 1 && (chart2.Series["Psi"].Points.Count > 100 || chart2.Series["Theta"].Points.Count > 100 || chart2.Series["Psi"].Points.Count > 100))
+            {
+                chart2.Series["Psi"].Points.RemoveAt(0);
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+          
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
